af0c915af843ef207962a08760d898f992ab024f
Arel Cordero <arel@iqengines.com>
2013-06-29 01:15:08 -0700

    added prompt to use custom SMTP server

--- piddy.py
+++ piddy.py
@@ -49,7 +49,58 @@ def wait_for_pid(pid):
     print "\nDone!"
 
 
-def notify(pid, recipients, name=None, sender=None):
+def prompt_for_credentials():
+    """
+    Prompts for user credentials for custom SMTP server.
+    Assumes TLS, port 587, for now.
+
+    Tests connection and exits if failure.
+    """
+
+    print "What is your SMTP user name (e.g., your email address)?"
+    user = raw_input("Username (or email): ").strip()
+    password = getpass.getpass()
+
+    default_host = "smtp." + user.split("@", 1)[1] if "@" in user else ""
+    host = raw_input("SMTP host name (%s): " % (default_host)) or default_host
+    host = host.strip()
+    port = 587
+
+    success = False
+    if user and host:
+        # Test connection
+        try:
+            s = smtplib.SMTP(host, port)
+            s.ehlo()
+            s.starttls()
+            s.ehlo()
+            result, message = s.login(user, password)
+            if result == 235:
+                success = True
+            else:
+                print message
+            s.quit()
+        except smtplib.SMTPException:
+            print "Could not authenticate to SMTP server"
+            sys.exit(1)
+        except smtplib.SMTPHeloError:
+            print "There was a problem communicating with the SMTP server."
+            sys.exit(1)
+        except RuntimeError:
+            print "Runtime error. Python does not support SSL/TLS"
+            sys.exit(1)
+
+    if success:
+        print "Credentials accepted."
+        return {"host": host, "port": port, "user": user, "password": password}
+    else:
+        print "Could not connect to (%s)." % host
+        sys.exit(1)
+
+
+
+def notify(pid, recipients, name=None, sender=None,
+           smtp_credentials=None):
     """
     Send email notification to list of recipients, regarding
     process (pid) completing.
@@ -65,9 +116,22 @@ def notify(pid, recipients, name=None, sender=None):
     msg['From'] = sender
     msg['To'] = ", ".join(recipients)
 
-    s = smtplib.SMTP('127.0.0.1')
-    s.sendmail(sender, recipients, msg.as_string())
-    s.quit()
+    if smtp_credentials == None:
+        # Send via localhost SMTP server
+        s = smtplib.SMTP('127.0.0.1')
+        s.sendmail(sender, recipients, msg.as_string())
+        s.quit()
+    else:
+        # Send via custom server (e.g., gmail)
+        # NOTE: TLS is assumed.
+        s = smtplib.SMTP(smtp_credentials["host"], smtp_credentials["port"])
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login(smtp_credentials["user"], smtp_credentials["password"])
+        s.sendmail(smtp_credentials["user"], recipients, msg.as_string())
+        s.quit()
+
     print "Sent email notification to %s." % (msg['To'])
 
 
@@ -83,6 +147,8 @@ if __name__ == "__main__":
                         help='human-readable name to give the process')
     parser.add_argument('-s', '--sender', type=str, default=None,
                         help='email address to send email from')
+    parser.add_argument('-p', '--prompt', action="store_true", default=False,
+                        help='prompt for SMTP credentials')
 
     # By default, print help and exit
     if len(sys.argv) == 1:
@@ -91,12 +157,19 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if check_pid(args.pid):
+
+        if args.prompt:
+            credentials = prompt_for_credentials()
+        else:
+            credentials = None
+
         wait_for_pid(args.pid)
 
         notify(args.pid,
                [s.strip() for s in args.email.split(',')],
                name=args.name,
-               sender=args.sender)
+               sender=args.sender,
+               smtp_credentials=credentials)
 
     else:
         print "Process id %d does not appear to be running." % args.pid
