5a1e936095bd88e22db1b123739ce7e61caa4717
Arel Cordero <arel@iqengines.com>
2013-06-29 12:46:21 -0700

    added flag for gmail; testing credentials

--- piddy.py
+++ piddy.py
@@ -42,15 +42,18 @@ def wait_for_pid(pid):
     Wait for process (pid) to complete before returning.
     """
 
-    t0 = time.time()
-    while check_pid(pid):
-        print "\rWaiting for pid %d to complete. (%d s)..." % (pid, time.time()-t0),
-        sys.stdout.flush()
-        time.sleep(1)
-    print "\nDone!"
+    try:
+        t0 = time.time()
+        while check_pid(pid):
+            print "\rWaiting for pid %d to complete. (%d s)..." % (pid, time.time()-t0),
+            sys.stdout.flush()
+            time.sleep(1)
+        print "\nDone!"
+    except (KeyboardInterrupt, SystemExit):
+        print "\nCanceled."
+        sys.exit(2)
 
-
-def prompt_for_credentials():
+def prompt_for_credentials(default_user="", default_host=""):
     """
     Prompts for user credentials for custom SMTP server.
     Assumes TLS, port 587, for now.
@@ -59,45 +62,51 @@ def prompt_for_credentials():
     """
 
     print "What is your SMTP user name (e.g., your email address)?"
-    user = raw_input("Username (or email): ").strip()
+    user = raw_input("Username (%s): " % default_user).strip() or default_user
     password = getpass.getpass()
 
-    default_host = "smtp." + user.split("@", 1)[1] if "@" in user else ""
+    default_host = default_host or ("smtp." + user.split("@", 1)[1] if "@" in user else "")
     host = raw_input("SMTP host name (%s): " % (default_host)) or default_host
     host = host.strip()
     port = 587
 
-    success = False
-    if user and host:
-        # Test connection
-        try:
-            s = smtplib.SMTP(host, port)
+    return {"host": host, "port": port, "user": user, "password": password}
+
+
+def test_credentials(credentials):
+    try:
+        if credentials:
+            # Test external server connection
+            s = smtplib.SMTP(credentials["host"], credentials["port"])
             s.ehlo()
             s.starttls()
             s.ehlo()
-            result, message = s.login(user, password)
-            if result == 235:
-                success = True
-            else:
-                print message
+            result, message = s.login(credentials["user"], credentials["password"])
             s.quit()
-        except smtplib.SMTPException:
-            print "Could not authenticate to SMTP server"
-            sys.exit(1)
-        except smtplib.SMTPHeloError:
-            print "There was a problem communicating with the SMTP server."
-            sys.exit(1)
-        except RuntimeError:
-            print "Runtime error. Python does not support SSL/TLS"
-            sys.exit(1)
+            if result != 235:
+                print message
+                raise
+        else:
+            # Test local server connection
+            s = smtplib.SMTP('127.0.0.1')
+            s.quit()
 
-    if success:
-        print "Credentials accepted."
-        return {"host": host, "port": port, "user": user, "password": password}
-    else:
-        print "Could not connect to (%s)." % host
+    except smtplib.SMTPException:
+        print "Could not authenticate to SMTP server"
+        sys.exit(1)
+    except smtplib.SMTPHeloError:
+        print "There was a problem communicating with the SMTP server."
+        sys.exit(1)
+    except RuntimeError:
+        print "Runtime error. Python does not support SSL/TLS"
+        sys.exit(1)
+    except:
+        host = credentials.get("host") if credentials else "127.0.0.1"
+        print "Could not connect to SMTP server (%s)." % host
         sys.exit(1)
 
+    print "Credentials accepted."
+    return True
 
 
 def notify(pid, recipients, name=None, sender=None,
@@ -150,6 +159,8 @@ if __name__ == "__main__":
                         help='email address to send email from')
     parser.add_argument('-p', '--prompt', action="store_true", default=False,
                         help='prompt for SMTP credentials')
+    parser.add_argument('-g', '--gmail', action="store_true", default=False,
+                        help='prompt for Gmail credentials')
 
     # By default, print help and exit
     if len(sys.argv) == 1:
@@ -159,15 +170,22 @@ if __name__ == "__main__":
 
     if check_pid(args.pid):
 
-        if args.prompt:
-            credentials = prompt_for_credentials()
+        recipients = [s.strip() for s in args.email.split(',')]
+        default_user = (args.sender or recipients[0])
+
+        if args.gmail:
+            credentials = prompt_for_credentials(default_user, "smtp.gmail.com")
+        elif args.prompt:
+            credentials = prompt_for_credentials(default_user)
         else:
             credentials = None
+
+        test_credentials(credentials)
 
         wait_for_pid(args.pid)
 
         notify(args.pid,
-               [s.strip() for s in args.email.split(',')],
+               recipients,
                name=args.name,
                sender=args.sender,
                smtp_credentials=credentials)
